5ee3b41bc2ba96d167d40cf76be7fbaa4cf92216
Ihsan <me@iganss.xyz>
2021-12-28 12:26:50 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,42 @@
-var cacheName = 'hello-world-page';
-var filesToCache = [
-  '/'
-];
+let CACHE_NAME = "IhsanGans";
+let urlsToCache = ["/"];
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
+// Install a service worker
+self.addEventListener("install", (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
+// Cache and return requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
+// Update a service worker
+self.addEventListener("activate", (event) => {
+  let cacheWhitelist = ["IhsanGans"];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
